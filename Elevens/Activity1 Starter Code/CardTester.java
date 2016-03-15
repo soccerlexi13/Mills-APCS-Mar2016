ff3910a64896f4958911784895de4c2bdcd39e61
soccerlexi13 <soccerlexi13@gmail.com>
2016-03-15 09:26:24 -0700

    :disappointed:

--- Elevens/Activity1 Starter Code/CardTester.java
+++ Elevens/Activity1 Starter Code/CardTester.java
@@ -3,11 +3,18 @@
  */
 public class CardTester {
 
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *  @param args is not used.
+     */
+    public static void main(String[] args) {
+        Card card1=new Card("Queen", "hearts", 10);
+        System.out.println(card1);
+        Card card2=new Card("3", "clubs", 3);
+        System.out.println(card2);
+        Card card3=new Card("9", "spades", 9);
+        System.out.println(card3);
+        Card card4=new Card("9", "spades", 9);
+        System.out.println(card3.matches(card4));
+    }
 }
--- Elevens/Activity2  Starter Code/DeckTester.java
+++ Elevens/Activity2  Starter Code/DeckTester.java
@@ -3,11 +3,14 @@
  */
 public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
-	}
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     *  @param args is not used.
+     */
+    public static void main(String[] args) {
+        String[] rankA={"Queen", "Jack", "8", "3"};
+        String[] suitA={"clubs", "hearts", "diamonds", "spades"};
+        int[] valueA={10, 10, 8, 3};
+        Deck deck1=new Deck(rankA, suitA, valueA);
+    }
 }
